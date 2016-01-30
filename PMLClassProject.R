727ce09ee8dca5637e8b6a2de1aa6f73b95e52e4
Dale Schroyer <dales@promodel.com>
2016-01-30 14:01:59 -0500

    Final Version of model creation and validation

--- PMLClassProject.R
+++ PMLClassProject.R
@@ -14,19 +14,21 @@ require(caret)
 
 # remove variables with nearly zero variance
 nzv <- nearZeroVar(trainingrawdata,saveMetrics=T)
-trainingdata <- trainingrawdata[, -nzv]
 nzv
+allrawdata <- trainingrawdata[, nzv$nzv==FALSE]
+
 
 # remove variables that are almost always NA
-mostlyNA <- sapply(trainingdata, function(x) mean(is.na(x))) > 0.95
-trainingdata <- trainingdata[, mostlyNA==F]
+mostlyNA <- sapply(allrawdata, function(x) mean(is.na(x))) > 0.95
+allrawdata <- allrawdata[, mostlyNA==FALSE]
 
 # remove variables that don't make intuitive sense for prediction (X, user_name, raw_timestamp_part_1, raw_timestamp_part_2, cvtd_timestamp), which happen to be the first five variables
-trainingdata <- trainingdata[, -(1:5)]
+allrawdata <- allrawdata[, -(1:5)]
+
 
 #now split into training and testing portions of the training data set
 set.seed(5846)
-index_train <- createDataPartition(y=trainingdata$classe, p=0.60, list=FALSE)
+index_train <- createDataPartition(y=allrawdata$classe, p=0.60, list=FALSE)
 data_train <- trainingdata[index_train,]
 data_xval <- trainingdata[-index_train,]
 dim(data_train); dim(data_xval)
@@ -34,10 +36,15 @@ dim(data_train); dim(data_xval)
 #let's look at the distribution of classes
 require(lattice)
 barchart(table(data_train$classe))
-
-#this still leaves us with 53 variable to predict the exercise class
-Correlations<-cor(data_train[,-54])
-diag(Correlations)<-0
-which(Correlations>.8,arr.ind=T)
-#standardize to reduce algoithum churn and simplify model building
-PreProcess<-preProcess(data_train[-54],method=c("center","scale"))
+#this looks like a pretty equal distribution so lets train the model
+#first choice is random forest since this is a clasification problem
+require(randomForest)
+modfit <-train(classe~.,data=data_train,method="rf",trControl=trainControl(method = "cv",number = 4,allowParallel = TRUE,verboseIter = TRUE))
+#check the model by cross validation
+pred_rf <- predict(modfit,data_xval)
+cm_rf <- confusionMatrix(pred_rf,data_xval$classe)
+cm_rf
+#Accuracy is over 99% 
+#test against final test set provided
+FinalValidationTest <- predict(modfit, testingrawdata)
+FinalValidationTest
